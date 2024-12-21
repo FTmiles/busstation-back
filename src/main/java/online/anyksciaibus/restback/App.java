b9e521347f76b8efe73bfa083a34702c1951e780
Swee X <phat@bro.com>
2024-12-21 22:32:40 +0200

    works on local - windows, WSL docker mySQL

--- src/main/java/online/anyksciaibus/restback/App.java
+++ src/main/java/online/anyksciaibus/restback/App.java
@@ -7,11 +7,11 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
 public class App extends SpringBootServletInitializer {
-//public class App {
-@Override
-protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-	return application.sources(App.class);
-}
+	//public class App {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(App.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
